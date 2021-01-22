0bb2b916929bc71d29fec133c79b8bb27ef8c8d9
Wino.Ltd <72581300+winoproject@users.noreply.github.com>
2021-01-22 06:00:01 +0530

    Update omni_lima.mk

--- omni_lima.mk
+++ omni_lima.mk
@@ -26,8 +26,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/motorola/lima/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/pb/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lima
